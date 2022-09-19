d0c32dbf7d4e8822e93a6d1eaf32f18b400f5377
Mark Jason <markjasongalang3@gmail.com>
2022-09-19 09:47:49 +0800

    third commit

--- app/src/main/java/com/fourbytes/loc8teapp/Signup2Activity.java
+++ app/src/main/java/com/fourbytes/loc8teapp/Signup2Activity.java
@@ -3,27 +3,46 @@ package com.fourbytes.loc8teapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Signup2Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Spinner spAccount;
     private LinearLayout llProOption;
+    private TextView tvValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup2);
 
+        // TODO: Test if an image can be retrieved from gallery
+
+        // Get views from the layout
         spAccount = findViewById(R.id.sp_account);
         llProOption = findViewById(R.id.ll_pro_option);
+        tvValidation = findViewById(R.id.tv_validation);
 
+        // Fill spinner with data
         initSpinnerAccount();
         spAccount.setOnItemSelectedListener(this);
+
+        // Put message for validation
+        initTextValidation();
+    }
+
+    private void initTextValidation() {
+        String message = "** validation process can take <b>3 - 5 business days</b>."+
+                        "you will receive an email once your account is verified. " +
+                        "<b>you may not use the account while it is in the process of verification</b>.";
+
+        tvValidation.setText(Html.fromHtml(message));
     }
 
     private void initSpinnerAccount() {
@@ -33,6 +52,7 @@ public class Signup2Activity extends AppCompatActivity implements AdapterView.On
         spAccount.setAdapter(adapter);
     }
 
+    /* The methods below are needed to be overridden for the spinner action listener. */
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
         if (pos == 0) {
