00c72e953bcc0062e0c830d33418d6b3e6ea6a3d
Mark Jason <markjasongalang3@gmail.com>
2022-09-22 21:34:39 +0800

    optimized imports

--- app/src/main/java/com/fourbytes/loc8teapp/LoginActivity.java
+++ app/src/main/java/com/fourbytes/loc8teapp/LoginActivity.java
@@ -3,18 +3,10 @@ package com.fourbytes.loc8teapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseFirestore db;
