65f137c8f891e26a6b5f8e0e429d53ea57047363
Angeline <201910405@fit.edu.ph>
2022-09-25 20:18:07 +0800

    added floating buttons to home fragment

--- app/src/main/java/com/fourbytes/loc8teapp/HomeActivity.java
+++ app/src/main/java/com/fourbytes/loc8teapp/HomeActivity.java
@@ -8,6 +8,8 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeActivity extends AppCompatActivity {
 
