99cc6cbb96837c60285126eb1be9f61c809bfe1f
Allen <allendelarosa456@gmail.com>
2022-09-30 19:44:11 +0800

    Added map

--- app/src/main/java/com/fourbytes/loc8teapp/fragment/FragmentHome_MapView.java
+++ app/src/main/java/com/fourbytes/loc8teapp/fragment/FragmentHome_MapView.java
@@ -1,16 +1,32 @@
 package com.fourbytes.loc8teapp.fragment;
 
+import static com.fourbytes.loc8teapp.Constants.MAPVIEW_BUNDLE_KEY;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.fourbytes.loc8teapp.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class FragmentHome_MapView extends Fragment {
+public class FragmentHome_MapView extends Fragment implements OnMapReadyCallback {
+    private View view;
+    private MapView map_view;
 
     public FragmentHome_MapView() {
         // Required empty public constructor
@@ -20,6 +36,89 @@ public class FragmentHome_MapView extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_map, container, false);
+        view = inflater.inflate(R.layout.fragment_home_map, container, false);
+        map_view = view.findViewById(R.id.map_view);
+
+        initGoogleMap(savedInstanceState);
+
+        return view;
+    }
+
+    private void initGoogleMap(Bundle savedInstanceState) {
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        map_view.onCreate(mapViewBundle);
+
+        map_view.getMapAsync(this);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+        map_view.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        googleMap.setMapType(1);
+        Log.d("MAPS: ", "True");
+        googleMap.setMyLocationEnabled(true);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map_view.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        map_view.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        map_view.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        map_view.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        map_view.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        map_view.onLowMemory();
     }
 }
