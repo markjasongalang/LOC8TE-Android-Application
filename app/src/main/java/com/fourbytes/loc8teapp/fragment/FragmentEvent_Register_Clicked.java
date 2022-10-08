0cbea870f774cf0baecbb0cf183236d788db1e4d
Allen <allendelarosa456@gmail.com>
2022-10-08 21:36:37 +0800

    Done general events except edit events

--- app/src/main/java/com/fourbytes/loc8teapp/fragment/FragmentEvent_Register_Clicked.java
+++ app/src/main/java/com/fourbytes/loc8teapp/fragment/FragmentEvent_Register_Clicked.java
@@ -11,11 +11,13 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.fourbytes.loc8teapp.R;
+import com.fourbytes.loc8teapp.fragment.professional.FragmentEvent_Industry_Professional;
 
 public class FragmentEvent_Register_Clicked extends Fragment {
     private View view;
 
     private AppCompatButton btnBack;
+    private AppCompatButton btnRegister;
     private FragmentManager fragmentManager;
 
     @Override
@@ -24,11 +26,23 @@ public class FragmentEvent_Register_Clicked extends Fragment {
 
         fragmentManager = getParentFragmentManager();
         btnBack = view.findViewById(R.id.btn_back);
+        btnRegister = view.findViewById(R.id.register_button);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 fragmentManager.popBackStack();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment, FragmentEvent_Register_Success.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack(null)
+                        .commit();
             }
         });
 
