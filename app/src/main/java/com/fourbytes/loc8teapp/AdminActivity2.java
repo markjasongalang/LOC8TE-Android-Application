9cc501177301b6fbd56ff3f063dc185670b3521e
Mark Jason <markjasongalang3@gmail.com>
2022-11-05 16:38:33 +0800

    commented out some lines in AdminActivity2 and renamed a drawable image

--- app/src/main/java/com/fourbytes/loc8teapp/AdminActivity2.java
+++ app/src/main/java/com/fourbytes/loc8teapp/AdminActivity2.java
@@ -1,8 +1,5 @@
 package com.fourbytes.loc8teapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -10,28 +7,22 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
-import org.w3c.dom.Text;
 
 public class AdminActivity2 extends AppCompatActivity {
 
@@ -42,7 +33,6 @@ public class AdminActivity2 extends AppCompatActivity {
     public Spinner sp_id_type;
     public ImageView image_ids;
 
-
     TextView Professional_name;
     TextView Professional_work;
     TextView Professional_first_name;
@@ -52,11 +42,6 @@ public class AdminActivity2 extends AppCompatActivity {
     ImageView Professional_profile_pic;
     ImageView Professional_id_pic;
 
-
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,11 +50,11 @@ public class AdminActivity2 extends AppCompatActivity {
         sp_id_type=findViewById(R.id.spinner_id_type);
         image_ids=findViewById(R.id.image_ids);
 
-        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-
-        DatabaseReference databaseReference = firebaseDatabase.getReference();
-
-        DatabaseReference getImage = databaseReference.child("profilePics");
+//        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+//
+//        DatabaseReference databaseReference = firebaseDatabase.getReference();
+//
+//        DatabaseReference getImage = databaseReference.child("profilePics");
 
         storage = FirebaseStorage.getInstance();
 
@@ -96,7 +81,7 @@ public class AdminActivity2 extends AppCompatActivity {
                         image_ids.setImageResource(R.drawable.loid);
                         break;
                     case 2:
-                        image_ids.setImageResource(R.drawable.driverslicense_template);
+                        image_ids.setImageResource(R.drawable.drivers_license_template);
                         break;
                     case 3:
                         image_ids.setImageResource(R.drawable.juswa_hearts);
